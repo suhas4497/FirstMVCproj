f7a11c4175a0a61e8756c8759db1032b929028c5
dev2 <dev2@gmail.com>
2024-11-14 12:13:37 +0530

    Add project files.

---
+++ FirstMVCproj/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FirstMVCproj.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
